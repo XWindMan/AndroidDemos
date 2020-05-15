5796b5560137481a41a2c4067efdaf71a5f3e533
windman <windman9191@gmail.com>
2020-05-16 00:12:41 +0800

    init project

---
+++ app/src/main/java/com/play/android_fast_dev/MainActivity.java
@@ -0,0 +1,14 @@
+package com.play.android_fast_dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
